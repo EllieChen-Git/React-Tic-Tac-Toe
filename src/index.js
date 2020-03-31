caf085ba769ee4697f2cec0dc425f2fd5f12cc3b
Ellie <elliechen.etc@gmail.com>
2020-03-31 14:57:21 +1100

    Implemented winner calculation with tie option

--- src/index.js
+++ src/index.js
@@ -2,17 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// Square component: renders a single <button>
-// class Square extends React.Component {
-//   render() {
-//     return (
-//       <button className="square" onClick={() => this.props.onClick()}>
-//         {this.props.value}
-//       </button>
-//     );
-//   }
-// }
-
 const Square = props => {
   return (
     <button className="square" onClick={props.onClick}>
@@ -30,11 +19,16 @@ class Board extends React.Component {
 
   handleClick = i => {
     const squares = this.state.squares.slice(); // array.slice: return a shallow copy of array, without modifying the original array
+
+    if (this.calculateWinner(squares) || squares[i]) {
+      // handleClick to return early by ignoring a click if someone has won the game or if a Square is already filled:
+      return;
+    }
     const { xIsNext } = this.state;
     squares[i] = xIsNext ? "X" : "O";
     this.setState({ squares, xIsNext: !xIsNext });
 
-    console.log(squares[i]);
+    console.log(squares);
   };
 
   renderSquare(i) {
@@ -46,9 +40,41 @@ class Board extends React.Component {
     );
   }
 
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   render() {
-    const { xIsNext } = this.state;
-    const status = `Next player: Player ${xIsNext ? "X" : "O"}`;
+    const { xIsNext, squares } = this.state;
+    const winner = this.calculateWinner(squares);
+    let status;
+    if (winner) {
+      status = `Congrutulations, Player ${winner} won!`;
+    } else if (!squares.includes(null)) {
+      status = `No one won this game`;
+    } else {
+      status = `Next player: Player ${xIsNext ? "X" : "O"}`;
+    }
 
     return (
       <div>
